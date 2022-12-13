0c240acfd71211ef13f5ca1299617b65a59018f2
zxliao <zhixiangleo@163.com>
2022-12-13 09:14:34 +0800

    first commit

---
+++ src/chapter02.m
@@ -0,0 +1,16 @@
+clear
+close all
+clc
+
+%% example 2.5
+% f = imread('../imgs/dipum_images_ch02/Fig0206(a)(rose-original).tif');
+% figure, imshow(f);
+% fp = f(end:-1:1,:);
+% figure, imshow(fp);
+% f2 = f(end:-1:1,end:-1:1);
+% figure, imshow(f2);
+% fc = f(257:768,257:768);
+% figure, imshow(fc);
+% fs = f(1:2:end,1:3:end);
+% figure, imshow(fs);
+
