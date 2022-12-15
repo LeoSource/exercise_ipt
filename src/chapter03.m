c4e3ce70d363eae88b0dda1eed0ad89224cf1c41
zxliao <zhixiangleo@163.com>
2022-12-15 09:43:36 +0800

    chore：数字图像处理matlab版第三章学习

--- src/chapter03.m
+++ src/chapter03.m
@@ -33,7 +33,7 @@ addpath('../dipum_1.1.3_mcodes/');
 % g = histeq(f,64);
 % figure, imshow(g);
 
-%% example3.5
+%% example 3.5
 % f = imread('../imgs/dipum_images_ch03/Fig0308(a)(pollen).tif');
 % figure, imshow(f);
 % figure, imhist(f);
@@ -48,14 +48,48 @@ addpath('../dipum_1.1.3_mcodes/');
 % xlabel('input intensity values');
 % ylabel('input intensity values');
 
-%% example3.6
-f = imread('../imgs/dipum_images_ch03/Fig0310(a)(Moon Phobos).tif');
+%% example 3.6
+% f = imread('../imgs/dipum_images_ch03/Fig0310(a)(Moon Phobos).tif');
+% figure, imshow(f);
+% figure, imhist(f);
+% f1 = histeq(f,255);
+% figure, imshow(f1);
+% figure, imhist(f1);
+% p = manualhist();
+% g = histeq(f,p);
+% figure, imshow(g);
+% figure, imhist(g);
+
+%% example 3.7
+% f = imread('../imgs/dipum_images_ch03/Fig0315(a)(original_test_pattern).tif');
+% figure, imshow(f);
+% w = ones(31);
+% gd = imfilter(im2double(f),w);
+% figure, imshow(gd, []);
+% gr = imfilter(im2double(f),w,'replicate');
+% figure, imshow(gr, []);
+% gc = imfilter(im2double(f),w,'circular');
+% figure, imshow(gc, []);
+% figure, imshow(imfilter(f,w,'replicate'));
+
+%% example 3.9 & 3.10
+% f = imread('../imgs/dipum_images_ch03/Fig0316(a)(moon).tif');
+% figure, imshow(f);
+% w = fspecial('laplacian', 0);
+% g1 = imfilter(im2double(f), w, 'replicate');
+% figure, imshow(g1, []);
+% figure, imshow(im2double(f)-g1);
+% 
+% w8 = [1,1,1;1,-8,1;1,1,1];
+% g2 = imfilter(im2double(f), w8, 'replicate');
+% figure, imshow(im2double(f)- g2);
+
+%% example 3.11
+f = imread('../imgs/dipum_images_ch03/Fig0318(a)(ckt-board-orig).tif');
 figure, imshow(f);
-figure, imhist(f);
-f1 = histeq(f,255);
-figure, imshow(f1);
-figure, imhist(f1);
-p = manualhist();
-g = histeq(f,p);
-figure, imshow(g);
-figure, imhist(g);
+fn = imnoise(f, 'salt & pepper', 0.2);
+figure, imshow(fn);
+gm = medfilt2(fn);
+figure, imshow(gm);
+figure, imshow(medfilt2(fn, 'symmetric'));
+
